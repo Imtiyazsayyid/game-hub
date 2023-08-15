d124c5688fe0305908d3dc740fb807a9110cdefe
Imtiyaz <imtiyazsayyidwork@gmail.com>
2023-08-16 00:39:36 +0530

    refactoring stale time

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,7 +1,7 @@
-import genres from "../data/genres";
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
+import genres from "../data/genres";
 import APIClient from "../services/api-client";
-import { FetchResponse } from "../services/api-client";
 
 export interface Genre {
   id: number;
@@ -15,7 +15,7 @@ const useGenres = () => {
   return useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, // 24 hours
+    staleTime: ms("24h"),
     initialData: genres,
   });
 };
